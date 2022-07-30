0a3821d5cc4791d705a9139d4d2344801ce15148
Alena Bagaeva <xaru123@yandex.ru>
2022-07-30 19:59:06 +0500

    clear

--- src/layout/authLayout/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import tpl from './tpl.hbs';
-import link from '../../components/link';
-
-Handlebars.registerPartial('auth-layout', tpl);
-
-export default (props) => {
-  return tpl(props);
-}
