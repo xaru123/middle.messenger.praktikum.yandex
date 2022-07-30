288b05e7979bbb61ac9605431c735af262476f40
Alena Bagaeva <xaru123@yandex.ru>
2022-07-30 20:21:20 +0500

    delete

--- src/components/icon/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import tpl from './tpl.hbs';
-import './style.scss';
-
-Handlebars.registerPartial('icon', tpl);
-
-export default (id, classIcon, onClick) => {
-  return tpl({id, classIcon, onClick});
-}
