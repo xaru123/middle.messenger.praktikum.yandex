fb0f942bf2d254e90a028c7bef5a321166e4b3b3
Alena Bagaeva <xaru123@yandex.ru>
2022-09-10 21:44:17 +0500

    't'

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-                                                import { router } from './router';
+import { router } from './router';
 
-                                                import ChangePassword from './pages/settings/changePassword';
-                                                import ChangeProfile from './pages/settings/changeProfile';
-                                                import Chats from './pages/chats/chats';
-                                                import Error400 from './pages/server/400';
-                                                import Error500 from './pages/server/500';
-                                                import Profile from './pages/settings/profile';
-                                                import SignIn from './pages/auth/signIn';
-                                                import SignUp from './pages/auth/signUp';
-                                                import { AuthController } from './controllers/auth';
+import ChangePassword from './pages/settings/changePassword';
+import ChangeProfile from './pages/settings/changeProfile';
+import Chats from './pages/chats/chats';
+import Error400 from './pages/server/400';
+import Error500 from './pages/server/500';
+import Profile from './pages/settings/profile';
+import SignIn from './pages/auth/signIn';
+import SignUp from './pages/auth/signUp';
+import { AuthController } from './controllers/auth';
 
-                                                import './style/styles.scss';
+import './style/styles.scss';
 
-                                                router
-                                                            .addFunctionForAuthCheck(new AuthController().checkUser)
-                                                  .use('/', new SignIn(), 'div', {}, false)
-                                                  .use('/sign-up', new SignUp(), 'div', {}, false)
-                                                  .use('/messenger', new Chats(), 'div', {}, true)
-                                                  .use('/settings', new Profile(), 'div', {}, true)
-                                                  .use('/settings/change/info', new ChangeProfile(), 'div', {}, true)
-                                                  .use('/settings/change/password', new ChangePassword(), 'div', {}, true)
-                                                  .use('/404', new Error400(), 'div', {}, false)
-                                                  .use('/500', new Error500(), 'div', {}, false)
-                                                  .start();
+router
+                          .addFunctionForAuthCheck(new AuthController().checkUser)
+  .use('/', new SignIn(), 'div', {}, false)
+  .use('/sign-up', new SignUp(), 'div', {}, false)
+  .use('/messenger', new Chats(), 'div', {}, true)
+  .use('/settings', new Profile(), 'div', {}, true)
+  .use('/settings/change/info', new ChangeProfile(), 'div', {}, true)
+  .use('/settings/change/password', new ChangePassword(), 'div', {}, true)
+  .use('/404', new Error400(), 'div', {}, false)
+  .use('/500', new Error500(), 'div', {}, false)
+  .start();
